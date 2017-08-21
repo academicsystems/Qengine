f54a1bf26a8bf25b1b758b21f0d8a17461761d93
picokol <serpicofossas@gmail.com>
2017-08-21 16:52:19 -0600

    missing moodle fix

--- qengine.py
+++ qengine.py
@@ -391,7 +391,7 @@ def start():
 	### FINAL RESPONSE ASSEMBLY ###
 
 	aesObj = AES.new(QENGINE_SALT, AES.MODE_CFB, QENGINE_IV)
-	stephtml = "<input type='hidden' name='%%IDPREFIX%%qform_data[temp.qengine.step]' value='" + base64.b64encode(aesObj.encrypt('0')) + "'>"
+	stephtml = "<input type='hidden' name='%%IDPREFIX%%temp.qengine.step' value='" + base64.b64encode(aesObj.encrypt('0')) + "'>"
 	
 	# assemble the final json response. progressInfo is set to the step number, so in this case it's 0, and process() will increment this
 	opData = {'CSS':qcss,'XHTML':qhtml + vhtml + stephtml,'progressInfo':0,'questionSession':qsessionID,'resources':genfiles}
@@ -554,7 +554,7 @@ def process(sid):
 		};
 	
 	aesObj = AES.new(QENGINE_SALT, AES.MODE_CFB, QENGINE_IV)
-	stephtml = "<input type='hidden' name='%%IDPREFIX%%qform_data[temp.qengine.step]' value='" + base64.b64encode(aesObj.encrypt(str(step))) + "'>"
+	stephtml = "<input type='hidden' name='%%IDPREFIX%%temp.qengine.step' value='" + base64.b64encode(aesObj.encrypt(str(step))) + "'>"
 	
 	opData = {'CSS':qcss,'XHTML':qhtml + vhtml + stephtml,'progressInfo':step,'questionEnd':questionEnd,'results':results,'resources':genfiles}
 
