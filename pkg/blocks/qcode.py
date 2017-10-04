9e6fe34c430a8ba3b74ab9dfa9831eeb7f6dae04
picokol <serpicofossas@gmail.com>
2017-10-03 20:24:25 -0600

    error reporting

--- pkg/blocks/qcode.py
+++ pkg/blocks/qcode.py
@@ -20,31 +20,34 @@ def process_any_code(key,code,reqvars,qenginevars,cachedresources,genfiles,quest
 	header = {'Content-Type':'application/json','Accept':'application/json'}
 	response = requests.post(CODE_URL + '/code',data = json.dumps(cjson),headers = header)
 	try:
-		qenginevars[key] = response.json()
+		qenginevars[key] = vars = response.json()
 	except:
 		question_errors.append('invalid response received from code service, ' + key)
+		return
 	
 	# retrieve any files that vars point to
-	for ns, vars in qenginevars.iteritems():
-		if ns != 'error':
-			for key, vals in vars.iteritems():
-				if key[0] == '_':
-					filename = vals[0]
-					if filename.split('.')[0] == 'error' or filename in cachedresources:
-						continue
-					
-					mt = mimetypes.guess_type(filename)[0]
-					response = requests.get(CODE_URL + '/static/' + filename) # request for file's binary content
-					if response.status_code == 404:
-						question_errors.append('unable to retrieve code generated resource, ' + filename)
-						pass
-					elif len(response.content) == 0:
-						question_errors.append('code generated resource is size 0, might indicate bad save code, ' + filename)
-						pass
-					else:
-						genfiles.append({
-							"content" : base64.b64encode(response.content),
-							"encoding" : "base64",
-							"filename" : filename,
-							"mimeType" : mt,
-						})
+	for key, vals in vars.iteritems():
+		if key == 'error':
+			question_errors.append('code block error: ' + vals[0])
+			continue
+		
+		if key[0] == '_':
+			filename = vals[0]
+			if filename.split('.')[0] == 'error' or filename in cachedresources:
+				continue
+			
+			mt = mimetypes.guess_type(filename)[0]
+			response = requests.get(CODE_URL + '/static/' + filename) # request for file's binary content
+			if response.status_code == 404:
+				question_errors.append('unable to retrieve code generated resource, ' + filename)
+				pass
+			elif len(response.content) == 0:
+				question_errors.append('code generated resource is size 0, might indicate bad save code, ' + filename)
+				pass
+			else:
+				genfiles.append({
+					"content" : base64.b64encode(response.content),
+					"encoding" : "base64",
+					"filename" : filename,
+					"mimeType" : mt,
+				})
